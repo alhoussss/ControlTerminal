f3ea7ac8411a1e003b0b55c2ba5959be10b3cfdf
alhoussss <mohamedsori.alhousseini@esqese.ucly.fr>
2023-02-22 17:48:04 +0100

    beginning

--- controlterminal/src/App.js
+++ controlterminal/src/App.js
@@ -1,9 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbare from "./navbar";
+
 
 function App() {
   return (
     <>
+      <Navbare/>
       <h1>Page 12</h1>
     </>
   );
