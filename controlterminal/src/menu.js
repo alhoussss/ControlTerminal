9b6b6cf11c7c87ab8664f734f64a39f221f28dd2
assi.karim <karimassi02@gmail.com>
2023-03-20 09:27:35 +0100

    desserts

--- controlterminal/src/menu.js
+++ controlterminal/src/menu.js
@@ -14,55 +14,55 @@ function Menu() {
       name: "Burgers",
       image:
         "https://www.forceetsaveur.com/wp-content/uploads/2019/10/food-burger-wallpaper-preview.jpg",
-      path: "/Menu/burgers"
+      path: "/Menu/burgers",
     },
     {
-      name: "Frites",
+      name: "Fries",
       image:
         "https://c4.wallpaperflare.com/wallpaper/666/302/422/food-potato-french-fries-hd-wallpaper-preview.jpg",
-      path: "/Menu/Fries"
+      path: "/Menu/Fries",
     },
     {
-      name: "Boissons",
+      name: "Drinks",
       image:
         "https://voyage-onirique.com/wp-content/uploads/2021/05/692432.jpg",
-        path: "/Menu/Boissons"
+      path: "/Menu/Drinks",
     },
     {
       name: "Desserts",
       image:
         "https://wallpapers.com/images/hd/assorted-cakes-and-cookies-dessert-s3nugcazto0oqwru.jpg",
-      path: "/Menu/Dessert"
+      path: "/Menu/Desserts",
     },
     {
-      name: "Salades",
+      name: "Salads",
       image:
         "https://img.freepik.com/photos-premium/salade-tomates-concombre-oignons-rouges-feuilles-laitue_2829-1732.jpg?w=2000",
-      path: "/Menu/Salades"
+      path: "/Menu/Salads",
     },
     {
       name: "Menus",
       image:
         "https://previews.123rf.com/images/angelmaxmixam/angelmaxmixam1601/angelmaxmixam160100056/50819649-fond-d-%C3%A9cran-menu-pour-les-pizzerias-et-restaurants-produits-de-l-image.jpg",
-      path: "/Menu/Menus"
+      path: "/Menu/Menus",
     },
     {
       name: "Sides",
       image:
         "https://images.unsplash.com/photo-1562059390-a761a084768e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8d3JhcHxlbnwwfHwwfHw%3D&w=1000&q=80",
-      path: "/Menu/Sides"
+      path: "/Menu/Sides",
     },
     {
       name: "Kids' Meals",
       image:
         "https://www.westrock.com/-/media/images/products/folding-carton/stock-box/stockboxkidsmeal-png-c-png.png",
-      path: "/Menu/KidsMeals"
+      path: "/Menu/KidsMeals",
     },
     {
       name: "Sauces",
       image:
         "https://www.mashed.com/img/gallery/which-fast-food-restaurant-has-the-best-dipping-sauce-heres-what-mashed-fans-say-exclusive-survey/l-intro-1662132318.jpg",
-      path: "/Menu/Sauces"
+      path: "/Menu/Sauces",
     },
   ];
 
