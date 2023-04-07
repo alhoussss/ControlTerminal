d03419a31f334f586d1b3c1724c40717e074f9c7
assi.karim <karimassi02@gmail.com>
2023-04-07 17:09:04 +0200

    priCEEE

--- controlterminal/src/Receive.js
+++ controlterminal/src/Receive.js
@@ -2,16 +2,12 @@ import React from "react";
 import "../src/CssFiles/Receive.css";
 import { NewContext } from "./Context";
 
-
-
-
-
-function Receive(){
-    const valeur = React.useContext(NewContext);
-    return(
-        <div className="cardR">
-            <h2> {valeur.Valeur} </h2>
-        </div>
-    )
+function Receive() {
+  const valeur = React.useContext(NewContext);
+  return (
+    <div className="cardR">
+      <h2> {valeur.Valeur} </h2>
+    </div>
+  );
 }
-export default Receive
+export default Receive;
