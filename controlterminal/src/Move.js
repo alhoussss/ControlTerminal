7ac9199ce5e8f5f65e336785161392eb327cde9d
alhoussss <mohamedsori.alhousseini@esqese.ucly.fr>
2023-04-11 11:15:53 +0200

    Partie admin finie

--- controlterminal/src/Move.js
+++ controlterminal/src/Move.js
@@ -3,12 +3,25 @@ import "./Move.css";
 
 function Move(){
     return(
+        <>
+        <div>
         <button className="Button"> 
-            <p>Subscribe</p>
-            <svg stroke-width="4" stroke="currentColor" viewBox="0 0 24 24" fill="none" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg">
-            <path d="M14 5l7 7m0 0l-7 7m7-7H3" stroke-linejoin="round" stroke-linecap="round"></path>
-            </svg>
+            <p>Next</p>
+            
         </button>
+        <button className="Buttons"> 
+            
+            <p>Back</p>
+           
+        </button>
+        </div>
+        </>
     )
 }
 export default Move;
+
+
+<button>
+  <svg height="16" width="16" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1024 1024"><path d="M874.690416 495.52477c0 11.2973-9.168824 20.466124-20.466124 20.466124l-604.773963 0 188.083679 188.083679c7.992021 7.992021 7.992021 20.947078 0 28.939099-4.001127 3.990894-9.240455 5.996574-14.46955 5.996574-5.239328 0-10.478655-1.995447-14.479783-5.996574l-223.00912-223.00912c-3.837398-3.837398-5.996574-9.046027-5.996574-14.46955 0-5.433756 2.159176-10.632151 5.996574-14.46955l223.019353-223.029586c7.992021-7.992021 20.957311-7.992021 28.949332 0 7.992021 8.002254 7.992021 20.957311 0 28.949332l-188.073446 188.073446 604.753497 0C865.521592 475.058646 874.690416 484.217237 874.690416 495.52477z"></path></svg>
+  <span>Back</span>
+</button>
